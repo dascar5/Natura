852b9eba439f7fa0d0e03be26edf5e323b80cdad
Bogdan Laban <bole.laban@yahoo.com>
2020-08-15 21:48:56 +0200

    Added email handlers

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,64 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: 'bec0828b9a2a1d',
-      pass: 'c29674b088d3d6',
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Bogdan Laban <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Bogdan Laban <bole.laban@yahoo.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  // 3) Actually send the email
-  await transport.sendMail(mailOptions);
+  async send(template, subject) {
+    // send the actual email
+    //1) saljemo pug stranicu kao sadrzaj mejla
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    //2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    //3) Create a transport and send email
+    this.newTransport();
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Dobrodošli u Natura porodicu!');
+  }
+
+  async sendPasswordReset() {
+    await this.send('passwordReset', 'Vaš token (validan samo 10 minuta)');
+  }
 };
-
-module.exports = sendEmail;
